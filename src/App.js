37ba85f7a3f64df0a72917fe8cd949a5d77b7af4
Iconic-It-Is <1devyansh1@gmail.com>
2022-06-30 16:11:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header.js";
+import Body from "./components/Body/Body.js";
+function App() {
+	return (
+		<div className="App">
+			<Header />
+			<Body />
+		</div>
+	);
+}
+
+export default App;
